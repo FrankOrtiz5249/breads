f1ad395fd6b64ac5fa63e40e7bb39724ad5a70ba
unknown <frankortiz5249@gmail.com>
2023-06-15 18:15:36 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,6 +16,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -26,6 +34,17 @@ breads.get('/:arrayIndex', (req, res) => {
     } else {
       res.send('404')
     }
+  })
+
+  //UPDATE
+  breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+      re2q.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
   })
 
 // CREATE
