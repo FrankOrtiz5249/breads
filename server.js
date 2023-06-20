f1cc4b6170534339ee2e343a4c062b7ace554324
unknown <frankortiz5249@gmail.com>
2023-06-19 18:28:08 -0700

    properly connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const methodOverride = require('method-override')
-
+const mongoose = require('mongoose')
 //CONFIG.
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -18,6 +18,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+    )
 
 //Breads
 const breadsController = require('./controllers/breads_controller.js')
